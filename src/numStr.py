4a639958335fb31b1f8772b02a5043f4b701f66e
Edo de Roo <git@edoderoo.nl>
2023-07-22 09:08:29 +0200

    adding works...

--- src/numStr.py
+++ src/numStr.py
@@ -6,27 +6,29 @@ class numStr:
     divide = []
 
     def addStr(self):
-      transfer = 0
-      resStr = ''
       for oneStr in self.add:
+         transfer = 0
+         resStr = ''
          if self.result=='': self.result='0' 
          cursorRes = len(self.result)
          cursorOne = len(oneStr)
-         while (cursorRes>0) and (cursorOne>0):
+         while (cursorRes>0) or (cursorOne>0):
             digit = transfer
             if cursorRes>0: digit += int(self.result[cursorRes-1:cursorRes])
             if cursorOne>0: digit += int(oneStr[cursorOne-1:cursorOne])
             transfer, digit = divmod(digit, 10)
-            resStr = chr(int('0') + digit) + resStr
+            resStr = chr(48 + digit) + resStr
             cursorOne -= 1
             cursorRes -= 1
-      self.result = resStr      
+         if transfer>0:
+            resStr = chr(48 + transfer) + resStr
+         self.result = resStr
 
     def calculate(self):
       if len(self.add) > 0:
          self.addStr()
 
 result = numStr()
-result.add = ['1234','5678']
+result.add = ['9999','99999']
 result.calculate()
 print(result.result)
