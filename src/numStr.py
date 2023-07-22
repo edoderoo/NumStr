39ff9a224bbf6eaf4d081c541999214afd1f5e70
Edo de Roo <git@edoderoo.nl>
2023-07-22 08:37:49 +0200

    add up structure

--- src/numStr.py
+++ src/numStr.py
@@ -6,8 +6,21 @@ class numStr:
     divide = []
 
     def addStr(self):
+      transfer = 0
+      resStr = ''
       for oneStr in self.add:
-         print(oneStr)
+         if self.result=='': self.result='0' 
+         cursorRes = len(self.result)
+         cursorOne = len(oneStr)
+         while (cursorRes>0) and (cursorOne>0):
+            digit = transfer
+            if cursorRes>0: digit += int(self.result[cursorRes-1:cursorRes])
+            if cursorOne>0: digit += int(oneStr[cursorOne-1:cursorOne])
+            transfer, digit = divmod(digit, 10)
+            resStr = chr(int('0') + digit) + resStr
+            cursorOne -= 1
+            cursorRes -= 1
+      self.result = resStr      
 
     def calculate(self):
       if len(self.add) > 0:
@@ -16,3 +29,4 @@ class numStr:
 result = numStr()
 result.add = ['1234','5678']
 result.calculate()
+print(result.result)
